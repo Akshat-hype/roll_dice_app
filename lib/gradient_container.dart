b1fe637150d2a749f1ab14ffc9ec417b166d5c03
Akshat-hype <akshatgoel310805@gmail.com>
2025-01-26 20:16:22 +0530

    added dice images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/styled_text.dart';
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
+var startalignment = Alignment.topLeft;
+var endalignment = Alignment.bottomRight;
 
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  void rollDice() {
+    //....
+  }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(225, 26, 2, 80),
-              Color.fromARGB(225, 45, 7, 98),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startalignment,
+          end: endalignment,
         ),
-        child: const Center(
-          child: StyledText(),
+      ),
+      child: Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
         ),
-      );
+        const SizedBox(height: 20),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28),
+            ),
+            child:const Text('Roll Dice'))
+      ])),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:roll_dice_app/gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer([
+              Color.fromARGB(225, 26, 2, 80),
+              Color.fromARGB(225, 45, 7, 98),
+            ]),
     ),
   ));
 }
